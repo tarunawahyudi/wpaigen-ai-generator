c81590dda9010c46a6b24757c230aafcdc03984c
tarunawahyudi <wahyuditaruna97@gmail.com>
2025-10-08 01:02:24 +0700

    fix: change base url api

--- includes/wpaigen-constants.php
+++ includes/wpaigen-constants.php
@@ -4,6 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-const WPAIGEN_BASE_API_URL = 'https://wp-ai-generator-api-production.up.railway.app';
+const WPAIGEN_BASE_API_URL = 'http://168.110.212.216:8100';
 const WPAIGEN_MIDTRANS_CLIENT_KEY = 'Mid-client-f_jUk6xl3gsdvIk3';
 const WPAIGEN_MIDTRANS_SNAP_URL= 'https://api.midtrans.com/snap/snap.js';
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: ai, content generator, article generator, seo, content creation
 Requires at least: 5.8
 Tested up to: 6.8
 Requires PHP: 7.4
-Stable tag: 2.1.0
+Stable tag: 3.1.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
