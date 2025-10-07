677730e03583d3c46acb2b8b23061aec4c02024f
tarunawahyudi <wahyuditaruna97@gmail.com>
2025-10-08 03:20:12 +0700

    update base url (2)

--- includes/wpaigen-constants.php
+++ includes/wpaigen-constants.php
@@ -4,6 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-const WPAIGEN_BASE_API_URL = 'http://168.110.212.216:8100';
+const WPAIGEN_BASE_API_URL = 'https://wpaigen-api.stacklab.id';
 const WPAIGEN_MIDTRANS_CLIENT_KEY = 'Mid-client-f_jUk6xl3gsdvIk3';
 const WPAIGEN_MIDTRANS_SNAP_URL= 'https://api.midtrans.com/snap/snap.js';
